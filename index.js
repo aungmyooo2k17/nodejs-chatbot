e1951d559b55fd4553b3e904643b4d89105dddbf
Aung <bigblackhacker2015@gmail.com>
2021-05-04 00:11:01 +0630

    init

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
+app.listen(3000, () => console.log('Chatbot server is listening, port 3000'));
 
 const verificationController = require('./controllers/verification');
 const messageWebhookController = require('./controllers/messageWebhook');
